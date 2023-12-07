3b9eb9eb87857e3a79bb5c3a6eaed121f82d4c27
andrew609-code <142052575+andrew609-code@users.noreply.github.com>
2023-12-07 10:30:47 -0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight: ");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package width: ");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height: ");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+        if (packageWeight > 50) 
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+        if (packageWidth + packageHeight > 50) 
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+        else 
+            {
+                int quote = (packageHeight * packageWeight * packageWidth) * packageWeight / 100;
+                Console.WriteLine("Your estimated total for shipping this package is: " + quote);
+                Console.ReadLine();
+            }
+
+        }            
+
+           
+                
+
+        
+        
+
+    }
+}
